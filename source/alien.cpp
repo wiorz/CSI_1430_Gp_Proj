599ec4cef808ce9549018b48fdda15e2e62ca4e5
Ivan Ko <kostivan+git@gmail.com>
2019-12-03 21:39:24 -0600

    Created alien class
    1. Implemented alien class.
    
    2. The alien is not in main yet, but has been tested somewhat. Waiting for the alienGroup class to be done so that it'll be moved into main.

---
+++ source/alien.cpp
@@ -0,0 +1,136 @@
+#include "alien.h"
+
+alien_t::alien_t()
+{
+    centerPos = point(10, 10);
+    radius = 15.0;
+    normal = GREENCOLOR;
+    cleared = BLACKCOLOR;
+    step = 10;
+    isAlive = true;
+    hitBox.setCenter(centerPos);
+    hitBox.setRadius(radius);
+    hitBox.setColor(normal);
+    totalCount++;
+}
+
+alien_t::alien_t(point p, int stepSize):centerPos(p), step(stepSize)
+{
+    radius = 15.0;
+    normal = GREENCOLOR;
+    cleared = BLACKCOLOR;
+    isAlive = true;
+    hitBox.setCenter(centerPos);
+    hitBox.setRadius(radius);
+    hitBox.setColor(normal);
+    totalCount++;
+}
+
+int alien_t::totalCount = 0;
+
+void alien_t::setCenterPos(point p)
+{
+    centerPos = p;
+}
+
+void alien_t::setRadius(double r)
+{
+    radius = r;
+}
+
+void alien_t::setStepSize(int s)
+{
+    step = s;
+}
+
+void alien_t::setNormalColor(color n)
+{
+    normal = n;
+}
+
+void alien_t::setClearedColor(color c)
+{
+    cleared = c;
+}
+
+void alien_t::makeAlive()
+{
+    if(!isAlive)
+    {
+        isAlive = true;
+        totalCount++;
+    }
+}
+
+
+void alien_t::kill()
+{
+    if(isAlive)
+    {
+        isAlive = false;
+        totalCount--;
+    }
+}
+
+void alien_t::moveByNStepsInXCoord(int numSteps)
+{
+    centerPos.x += numSteps * step;
+    hitBox.setCenter(centerPos);
+}
+
+void alien_t::moveByNStepsInYCoord(int numSteps)
+{
+    centerPos.y += numSteps * step;
+    hitBox.setCenter(centerPos);
+}
+
+point alien_t::getCenterPos() const
+{
+    return centerPos;
+}
+
+double alien_t::getRadius() const
+{
+    return radius;
+}
+
+int alien_t::getStepSize()const
+{
+    return step;
+}
+
+color alien_t::getNormalColor() const
+{
+    return normal;
+}
+
+color alien_t::getClearedColor() const
+{
+    return cleared;
+}
+
+bool alien_t::getIsAlive() const
+{
+    return isAlive;
+}
+
+// TODO: draw base on something else, NOT from hitbox.
+void alien_t::undraw(SDL_Plotter& g)
+{
+    hitBox.setColor(cleared);
+    hitBox.draw(g);
+}
+
+void alien_t::draw(SDL_Plotter& g)
+{
+    if(isAlive)
+    {
+        hitBox.setColor(normal);
+        hitBox.draw(g);
+    } else
+    {
+        undraw(g);
+    }
+
+}
+
--- source/main.cpp
+++ source/main.cpp
@@ -2,12 +2,13 @@
 
 #include <iostream>
 #include <ctime>
+#include "SDL_Plotter.h"
 #include "point.h"
 #include "line.h"
 #include "rectangle.h"
 #include "circle.h"
 #include "player.h"
-#include "SDL_Plotter.h"
+#include "alien.h"
 
 using namespace std;
 
@@ -21,11 +22,14 @@ int main(int argc, char* argv[])
     srand(time(0));
     char key;
     player p(g, SPEED);
+    alien_t a1(point(g.getCol()/2, g.getRow()/2), SPEED);
 
     while(!g.getQuit())
     {
         if(g.kbhit())
         {
+
+            cout << alien_t::totalCount << endl;
 
             key = g.getKey();
 
@@ -38,8 +42,6 @@ int main(int argc, char* argv[])
             // "Erase" previous rectangle by setting it to background color
             // Shows nothing with any key input.
             p.undraw(g);
-
-
 
 
             // Step 2.
@@ -59,12 +61,10 @@ int main(int argc, char* argv[])
                 case LEFT_ARROW:
                     if(p.getBodyRectangle().getUpperLeft().x > 0)
                     {
-
                         p.movePlayerByNSteps(-1);
                     }
                     break;
                 case UP_ARROW:
-                    //speed++;
                     break;
                 case DOWN_ARROW:
                     break;
